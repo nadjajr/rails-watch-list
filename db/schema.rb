ae1c94cc59c1f811b8db330e8e13617685933a6b
Nadja Josefin Rünzel <nadja.ruenzel@gmail.com>
2023-08-18 12:49:30 +0200

    Change the movie rating from integer to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_18_094640) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_104511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_094640) do
     t.string "title"
     t.string "overview"
     t.string "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
